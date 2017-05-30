d991dadb716d34e9490faa65f865c78c24f946d0
Michael Padget <mpadget@comcast.net>
2017-05-30 11:39:07 -0700

    Added test setup

--- lab-padget/karma.conf.js
+++ lab-padget/karma.conf.js
@@ -1,81 +1,35 @@
-// Karma configuration
-// Generated on Thu May 25 2017 10:53:17 GMT-0700 (PDT)
-
 const webpack = require('./webpack.config.js');
 delete webpack.entry;
 
 module.exports = function(config) {
   config.set({
     webpack,
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
-    // list of files / patterns to load in the browser
     files: [
-      'app/index.js',
+      'entry.js',
       'test/**/*-test.js',
     ],
 
-
-    // list of files to exclude
     exclude: [
       'no',
     ],
 
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'app/index.js': ['webpack'],
+      'entry.js': ['webpack'],
       'test/**/*-test.js': ['webpack'],
     },
-    // before we use these files for our tests, we need to run webpack on them.
 
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    // reporters: ['progress'],
-    reporters: ['mocha'], // googe: karma mocha reporter as -D
-
-
-    // web server port
+    reporters: ['mocha'],
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
-    // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-    // can change to debug. try logging config.
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-    // can search: chrome headless, phantom js.
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
-    // true
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity,
   });
 };
