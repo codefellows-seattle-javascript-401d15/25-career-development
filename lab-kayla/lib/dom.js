64a6f61a1dbf925dfd5e46ad2780d15fc3e5035a
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-06-30 21:37:59 -0700

    dom

--- lab-kayla/lib/dom.js
+++ lab-kayla/lib/dom.js
@@ -1,18 +1,65 @@
 'use strict'
 
-const fs = require('fs')
+const readHTML = require('./fs-read.js')
+const Tree = require('./tree')
 
-const readHTML = module.exports = function(file, cb) {
+const createDOM = module.exports = function() {
+  readHTMl(`${__dirname}/../assets/minimal.html`, function(err, data) {
 
-  fs.readFile(file, function(err, data) {
-    if(err) throw Error
+    if(err) throw err
 
-    let newData = data.toString().split('\n')
-    newData.shift()
+    let legend = {
+      html: data[0],
+      head: data[1],
+      title: data[2],
+      body: data[3],
+      header: data[4],
+      h2: data[5],
+      nav: data[6],
+      ul: [7],
+      li1: [8],
+      li2: [9],
+      main: [10],
+      section: [11],
+      sectP1: [12],
+      sectP2: data[13]
+    }
 
-    newData = newData.map(function(i) {
-      return i.trim()
+    let htmlTree = new Tree()
+
+    htmlTree.add(legend.html)
+
+    //root
+    htmlTree.add(legend.head, legend.html)
+    htmlTree.add(legend.body, legend.html)
+
+    //head
+    htmlTree.add(legend.title, legend.head)
+
+    //body
+    htmlTree.add(legend.header, legend.body)
+    htmlTree.add(legend.main, legend.body)
+
+    //header
+    htmlTree.add(legend.h2, legend.header)
+    htmlTree.add(legend.nav, legend.header)
+
+    //nav
+    htmlTree.add(legend.ul, legend.nav)
+
+    //ul
+    htmlTree.add(legend.li1, legend.ul)
+    htmlTree.add(legend.li2, legend.ul)
+
+    //main
+    htmlTree.add(legend.section, legend.main)
+
+    //section
+    htmlTree.add(legend.sec1, legend.section)
+    htmlTree.add(legend.sec2, legend.section)
+
+    htmlTree.preOrder(function print(node) {
+      console.log(node)
     })
-    cb(null, newData)
   })
 }
